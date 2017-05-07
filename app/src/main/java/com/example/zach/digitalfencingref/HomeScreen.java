c7b326ecc6918d26544b8e368c6bd498cc26df73
Zach <allegretti813@gmail.com>
2017-05-07 11:30:19 -0400

    implemented count down timer. Still needs additional testing. Start/Stop buttons not responding

--- app/src/main/java/com/example/zach/digitalfencingref/HomeScreen.java
+++ app/src/main/java/com/example/zach/digitalfencingref/HomeScreen.java
@@ -1,20 +1,28 @@
 package com.example.zach.digitalfencingref;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.concurrent.TimeUnit;
+
 public  class HomeScreen extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    private CountDownTimer countDownTimer;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -42,14 +50,19 @@ public  class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+
+
         ImageButton redPlus = (ImageButton) findViewById(R.id.redPlus);
         ImageButton redMinus = (ImageButton) findViewById(R.id.redMinus);
         ImageButton greenPlus = (ImageButton) findViewById(R.id.greenPlus);
         ImageButton greenMinus = (ImageButton) findViewById(R.id.greenMinus);
+        Button start = (Button) findViewById(R.id.start);
+        Button stop = (Button) findViewById(R.id.stop);
 
         final TextView redScore = (TextView) findViewById(R.id.redScore);
         final TextView greenScore = (TextView) findViewById(R.id.greenScore);
-        TextView timer = (TextView) findViewById(R.id.timer);
+        final TextView time = (TextView) findViewById(R.id.time);
+
 
         redPlus.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,9 +97,41 @@ public  class HomeScreen extends AppCompatActivity {
             }
         });
 
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentTime = time.getText().toString();
+                int minutes = Integer.parseInt(currentTime.substring(0,currentTime.indexOf(":")));
+                int seconds = Integer.parseInt(currentTime.substring(currentTime.indexOf(":") + 1));
+                long minutesToMilli = TimeUnit.MINUTES.toMillis(minutes);
+                long secondsToMilli = TimeUnit.SECONDS.toMillis(seconds);
+                long total = minutesToMilli + secondsToMilli;
+                 countDownTimer = new CountDownTimer(total,1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        String currentTime = String.format("%02d:%02d", TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)), TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
+                        time.setText(currentTime);
+                    }
+
+                    @Override
+                    public void onFinish() {
+
+                    }
+                };
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.cancel();
+            }
+        });
+
+
 
 
 
     }
+
 
 }
